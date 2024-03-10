966d86dfd030ad51bf6fa51ca6b8293475a25c89
Khanh Nguyen <nguyenckhanh71@gmail.com>
2024-03-10 16:10:29 +0700

    BA-4: feat: Notification Screen

--- app/src/main/java/com/example/bookingapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/bookingapp/ui/theme/Theme.kt
@@ -10,22 +10,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Orange,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Orange,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
+    background = Color(0xFFF9F9F9),
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
